7bc7e2d4a1358f7e4bd74669328ee0455b4efa98
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-09-04 21:24:11 -0500

    build out Home.js

---
+++ src/components/HeroSection.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button } from './Button'
+import '../App.css'
+import './HeroSection.css'
+
+function HeroSection() {
+    return (
+        <div className="hero-container">
+            <video src="/videos/flowers.mp4" autoPlay loop muted />
+            <h1>Welcome to Roaring Grace</h1>
+            <p>Let's Breathe</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle="btn--outline" buttonSize="btn--large">Begin</Button>
+                <Button className="btns" buttonStyle="btn--primary" buttonSize="btn--large">Wander</Button>
+            </div>
+        </div>
+    )
+}
+
+export default HeroSection
