150e46f1b37a5343facb8e3b224c21e74191e443
JohnnyMohawk <johnnelsonalden@gmail.com>
2021-09-04 22:44:41 -0500

    build out cards.js

--- src/components/pages/Cards.js
+++ src/components/pages/Cards.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import CardItem from './CardItem'
+import './Cards.css'
 
 function Cards() {
     return (
@@ -8,7 +9,14 @@ function Cards() {
             <div className="cards__container">
                 <div className="cards__wrapper">
                     <ul className="cards__items">
-                        <CardItem />
+                        <CardItem src="images/waterfall.jpeg" text="Blossom into your BEST self!" label="Spiritual" path="/spiritual" />
+                        <CardItem src="images/waterfall.jpeg" text="Daily Affirmations: For a good day" label="Spiritual" path="/spiritual" />
+                        <CardItem src="images/yoga-mountains.jpeg" text="Blossom into your BEST self!" label="Spiritual" path="/spiritual" />
+                    </ul>
+                    <ul className="cards__items">
+                        <CardItem src="images/yoga-mountains.jpeg" text="Blossom into your BEST self!" label="Spiritual" path="/spiritual" />
+                        <CardItem src="images/yoga-mountains.jpeg" text="Daily Affirmations: For a good day" label="Spiritual" path="/spiritual" />
+                        <CardItem src="images/yoga-mountains.jpeg" text="Daily Affirmations: For a good day" label="Spiritual" path="/spiritual" />
                     </ul>
                 </div>
             </div>
